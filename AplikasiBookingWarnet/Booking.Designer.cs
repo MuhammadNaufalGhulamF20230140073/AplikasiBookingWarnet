a0c85722c36018c401d9af6ac38bd7ba50c57193
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2025-04-23 04:00:47 +0700

    membuat namespacebutton

--- AplikasiBookingWarnet/Booking.Designer.cs
+++ AplikasiBookingWarnet/Booking.Designer.cs
@@ -56,3 +56,107 @@
         this.btnPesan.Text = "Pesan";
         this.btnPesan.UseVisualStyleBackColor = true;
         this.btnPesan.Click += new System.EventHandler(this.btnPesan_Click);
+        // btnBatal
+        // 
+        this.btnBatal.Location = new System.Drawing.Point(169, 114);
+        this.btnBatal.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+        this.btnBatal.Name = "btnBatal";
+        this.btnBatal.Size = new System.Drawing.Size(71, 24);
+        this.btnBatal.TabIndex = 5;
+        this.btnBatal.Text = "Batal";
+        this.btnBatal.UseVisualStyleBackColor = true;
+        this.btnBatal.Click += new System.EventHandler(this.btnBatal_Click);
+        //         // label1
+        // 
+        this.label1.AutoSize = true;
+        this.label1.Location = new System.Drawing.Point(19, 11);
+        this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+        this.label1.Name = "label1";
+        this.label1.Size = new System.Drawing.Size(63, 13);
+        this.label1.TabIndex = 6;
+        this.label1.Text = "Booking ID:";
+        // 
+        // label2
+        // 
+        this.label2.AutoSize = true;
+        this.label2.Location = new System.Drawing.Point(19, 35);
+        this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+        this.label2.Name = "label2";
+        this.label2.Size = new System.Drawing.Size(58, 13);
+        this.label2.TabIndex = 7;
+        this.label2.Text = "Username:";
+        // 
+        // label3
+        // 
+        this.label3.AutoSize = true;
+        this.label3.Location = new System.Drawing.Point(19, 59);
+        this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+        this.label3.Name = "label3";
+        this.label3.Size = new System.Drawing.Size(58, 13);
+        this.label3.TabIndex = 8;
+        this.label3.Text = "Device ID:";
+        // 
+        // label4
+        // 
+        this.label4.AutoSize = true;
+        this.label4.Location = new System.Drawing.Point(19, 84);
+        this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+        this.label4.Name = "label4";
+        this.label4.Size = new System.Drawing.Size(42, 13);
+        this.label4.TabIndex = 9;
+        this.label4.Text = "Waktu:";
+        // 
+        // dgvBooking
+        this.dgvBooking.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+        this.dgvBooking.Location = new System.Drawing.Point(25, 190);
+        this.dgvBooking.Name = "dgvBooking";
+        this.dgvBooking.RowHeadersWidth = 51;
+        this.dgvBooking.RowTemplate.Height = 24;
+        this.dgvBooking.Size = new System.Drawing.Size(500, 200);
+        this.dgvBooking.TabIndex = 10;
+
+        // FormBooking
+        this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        this.ClientSize = new System.Drawing.Size(560, 420);
+        this.Controls.Add(this.dgvBooking);
+        this.Controls.Add(this.label4);
+        this.Controls.Add(this.label3);
+        this.Controls.Add(this.label2);
+        this.Controls.Add(this.label1);
+        this.Controls.Add(this.btnBatal);
+        this.Controls.Add(this.btnPesan);
+        this.Controls.Add(this.txtbookingID);
+        this.Controls.Add(this.txtUsername);
+        this.Controls.Add(this.txtDeviceID);
+        this.Controls.Add(this.txtWaktu);
+        this.Name = "FormBooking";
+        this.Text = "Booking PC";
+        this.Load += new System.EventHandler(this.FormBooking_Load);
+        ((System.ComponentModel.ISupportInitialize)(this.dgvBooking)).EndInit();
+        this.ResumeLayout(false);
+        this.PerformLayout();
+    }
+    
+            // FormBooking
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(560, 420);
+            this.Controls.Add(this.dgvBooking);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnBatal);
+            this.Controls.Add(this.btnPesan);
+            this.Controls.Add(this.txtbookingID);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.txtDeviceID);
+            this.Controls.Add(this.txtWaktu);
+            this.Name = "FormBooking";
+            this.Text = "Booking PC";
+            this.Load += new System.EventHandler(this.FormBooking_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBooking)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+}
--- AplikasiBookingWarnet/Booking.cs
+++ AplikasiBookingWarnet/Booking.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -10,20 +11,3 @@ using System.Windows.Forms;
 
 namespace booking
 {
-    public partial class Booking: Form
-    {
-        private string connectionString = "Data Source=DESKTOP-4D54309;Initial Catalog=bookingpc;Integrated Security=True";
-        private string loggedInUsername;
-
-        public FormBooking(string username)
-        {
-            InitializeComponent();
-            loggedInUsername = username;
-        }
-        private void FormBooking_Load(object sender, EventArgs e)
-        {
-            txtUsername.Text = loggedInUsername;
-            txtUsername.ReadOnly = true;
-            txtbookingID.Text = GenerateBookingID();
-            txtbookingID.ReadOnly = true;
-        }
