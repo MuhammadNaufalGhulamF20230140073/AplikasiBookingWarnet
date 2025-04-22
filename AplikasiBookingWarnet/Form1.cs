1576b0518ec325e554cfcfbb9586791c580331e5
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2025-04-23 02:22:37 +0700

    untuk ambil data dari database menggunakan SqlConnection

--- AplikasiBookingWarnet/Form1.cs
+++ AplikasiBookingWarnet/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -22,3 +23,7 @@ namespace AplikasiBookingWarnet
         {
             LoadData();
         }
+        private void LoadData()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
